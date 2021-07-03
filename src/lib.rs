5e7817fc8b53f926ee147d55a2a389edfc1709c8
angelocat <catalaniangelo@gmail.com>
2021-07-03 22:16:23 +0200

    style: sort imports

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
+pub use permutations::Permutations;
 pub use permutations::into_chunks::Chunk;
 pub use permutations::into_chunks::IntoChunks;
-pub use permutations::into_optimized_chunks::IntoOptimizedChunks;
 pub use permutations::into_optimized_chunks::OptimizedChunk;
-pub use permutations::Permutations;
+pub use permutations::into_optimized_chunks::IntoOptimizedChunks;
 
 mod permutations;
